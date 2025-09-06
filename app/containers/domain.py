0afbbadb2664566bd05a65bdc805abbe54b0573a
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-09-06 18:43:30 +0900

    [ feat ] DatasetService 등록 ( #6 )
    - 도메인 컨테이너에 DatasetService를 등록하고, 해당 서비스에 **S3 의존성**을 주입

--- app/containers/domain.py
+++ app/containers/domain.py
@@ -1,4 +1,13 @@
 from dependency_injector import containers, providers
 
+from app.service.dataset_service import DatasetService
+
+
 class DomainsContainer(containers.DeclarativeContainer):
     infra = providers.DependenciesContainer()
+
+    # Service
+    dataset_service = providers.Factory(
+        DatasetService,
+        s3=infra.s3,
+    )
