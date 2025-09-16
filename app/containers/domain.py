ac74035067d2d619bd995361cd4d023f63c20547
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-09-16 22:48:11 +0900

    [ feat ] Outbox/Redis 스트림 의존성 추가 ( #13 )

--- app/containers/domain.py
+++ app/containers/domain.py
@@ -1,8 +1,12 @@
 from dependency_injector import containers, providers
 
 from app.crud.dataset_crud import DatasetCRUD
+from app.crud.outbox_crud import OutboxCRUD
 from app.rag.chain import build_rag_chain
 from app.service.dataset_service import DatasetService
+from app.service.outbox_service import OutboxService
+from app.service.streams.indexer import StreamIndexer
+from app.service.streams.redis_stream import RedisStream
 
 
 class DomainsContainer(containers.DeclarativeContainer):
@@ -14,11 +18,22 @@ class DomainsContainer(containers.DeclarativeContainer):
         session=infra.session,
     )
 
+    outbox_crud = providers.Factory(
+        OutboxCRUD,
+        session=infra.session,
+    )
+
     # Service
+    outbox_service = providers.Factory(
+        OutboxService,
+        outbox_crud=outbox_crud,
+    )
+
     dataset_service = providers.Factory(
         DatasetService,
         s3=infra.s3,
-        crud=dataset_crud,
+        dataset_crud=dataset_crud,
+        outbox_service=outbox_service,
         vectorstore=infra.chroma,
     )
 
@@ -27,4 +42,18 @@ class DomainsContainer(containers.DeclarativeContainer):
         build_rag_chain,
         vectorstore=infra.chroma,
         llm=infra.llm,
+    )
+
+    # Reids-Stream
+    redis_stream = providers.Factory(
+        RedisStream,
+        redis=infra.redis,
+    )
+    
+    stream_indexer = providers.Factory(
+        StreamIndexer,
+        outbox_service=outbox_service,
+        stream=redis_stream,
+        s3=infra.s3,
+        vectorstore=infra.chroma,
     )
